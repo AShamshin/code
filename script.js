dc329675cd195025401fb5bf4da6d5afc821011a
AShamshin <albert.shamshin@mail.ru>
2022-12-24 17:48:17 +0300

    Quarter of the year

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-function shortcut(string) {
-  let c = '';
-
-  for (let a = 0; a < string.length; a++) {
-    let b = string[a];
-
-    if (b == 'a' || b == 'e' || b == 'i' || b == 'o' || b == 'u') continue;
-    c += b;
+const quarterOf = (month) => {
+  let a = 0;
+  switch (month) {
+    case 1:
+    case 2:
+    case 3:
+      a = 1;
+      break;
+    case 4:
+    case 5:
+    case 6:
+      a = 2;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      a = 3;
+      break;
+    case 10:
+    case 11:
+    case 12:
+      a = 4;
+      break;
+    default:
+      alert('Нет таких значений');
   }
 
-  return c;
-}
-shortcut('hello');
+  return a;
+};
