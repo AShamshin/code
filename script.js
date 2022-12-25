640fb53d24124fcd81467f3eb9d01498af6eefb9
AShamshin <albert.shamshin@mail.ru>
2022-12-25 12:29:45 +0300

    saleHotdogs

--- script.js
+++ script.js
@@ -1,29 +1,13 @@
-const quarterOf = (month) => {
-  let a = 0;
-  switch (month) {
-    case 1:
-    case 2:
-    case 3:
-      a = 1;
-      break;
-    case 4:
-    case 5:
-    case 6:
-      a = 2;
-      break;
-    case 7:
-    case 8:
-    case 9:
-      a = 3;
-      break;
-    case 10:
-    case 11:
-    case 12:
-      a = 4;
-      break;
-    default:
-      alert('Нет таких значений');
+function saleHotdogs(n) {
+  let i = 0;
+  if (n < 5) {
+    i = n * 100;
+  } else if (n >= 5 && n < 10) {
+    i = n * 95;
+  } else if (n >= 10) {
+    i = n * 90;
   }
 
-  return a;
-};
+  return i;
+}
+alert(saleHotdogs(10));
