4c5370a0fd6f3ed1a53a6de1bb254aba823e06e1
AShamshin <albert.shamshin@mail.ru>
2022-12-26 08:51:40 +0300

    howManyLightsabersDoYouOwn

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const stringToNumber = function (str) {
-  let n = +str;
-  return n;
-};
+function howManyLightsabersDoYouOwn(name) {
+  let a;
+
+  if (name == 'Zach') {
+    a = 18;
+  } else {
+    a = 0;
+  }
+
+  return alert(a);
+}
+howManyLightsabersDoYouOwn('Zach');
