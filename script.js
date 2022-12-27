6c111146d0f2ea20620beb496e51161f7c8507b0
AShamshin <albert.shamshin@mail.ru>
2022-12-27 10:28:29 +0300

    getAge

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function findDifference(a, b) {
-  let c = a[0] * a[1] * a[2];
-  let d = b[0] * b[1] * b[2];
-  if (c > d) return c - d;
-  else {
-    return d - c;
-  }
+function getAge(inputString) {
+  let a = inputString[0];
+  return +a;
 }
