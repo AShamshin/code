bc828732b42787bf9d54d7139849a2598e914b71
AShamshin <albert.shamshin@mail.ru>
2022-12-27 10:27:08 +0300

    findDifference

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function multiply(a, b) {
-  a * b;
-  return alert(a * b);
+function findDifference(a, b) {
+  let c = a[0] * a[1] * a[2];
+  let d = b[0] * b[1] * b[2];
+  if (c > d) return c - d;
+  else {
+    return d - c;
+  }
 }
-multiply(3, 5);
