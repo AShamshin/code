2e840e9d14865a3b69d114d1ff1d16b1a329769c
AShamshin <albert.shamshin@mail.ru>
2022-12-28 09:36:52 +0300

    switchItUp

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
-function problem(x) {
-  if (typeof x == 'string') {
-    return 'Error';
-  } else {
-    return x * 50 + 6;
+function switchItUp(number) {
+  switch (number) {
+    case 0:
+      return 'Zero';
+
+    case 1:
+      return 'One';
+
+    case 2:
+      return 'Two';
+
+    case 3:
+      return 'Three';
+
+    case 4:
+      return 'Four';
+
+    case 5:
+      return 'Five';
+
+    case 6:
+      return 'Six';
+
+    case 7:
+      return 'Seven';
+
+    case 8:
+      return 'Eight';
+
+    case 9:
+      return 'Nine';
   }
 }
-console.log(problem(5));
+console.log(switchItUp(4));
