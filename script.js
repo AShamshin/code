0c75b40402d659486a9e51ee1ecd18d47b417fde
AShamshin <albert.shamshin@mail.ru>
2022-12-29 09:52:37 +0300

    fixTheMeerkat

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function apple(x) {
-  if (x * x > 1000) {
-    return "It's hotter than the sun!!";
-  } else {
-    return 'Help yourself to a honeycomb Yorkie for the glovebox.';
-  }
+function fixTheMeerkat(arr) {
+  return arr.reverse();
 }
-console.log(apple('90'));
+console.log(fixTheMeerkat(['tail', 'body', 'head']));
