0ba53a4d73cd38112b19aa76e047059cdce3e256
AShamshin <albert.shamshin@mail.ru>
2022-12-30 11:06:02 +0300

    check

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function addFive(num) {
-  var total = num + 5;
-  return total;
+function check(a, x) {
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] === x) {
+      return true;
+    }
+  }
+  return false;
 }
+
+console.log(check(['t', 'e', 's', 't'], 'e'));
