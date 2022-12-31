9be6dd462c0419a16131dcd44f995195dda07711
AShamshin <albert.shamshin@mail.ru>
2022-12-31 09:20:50 +0300

    countPositivesSumNegatives

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-const evenOrOdd = (number) => (number % 2 ? 'Odd' : 'Even');
+function countPositivesSumNegatives(input) {
+  let a = 0;
+  let b = 0;
+  let c = [];
+  if (input?.length === 0 || input === null) {
+    return [];
+  }
+  for (let i = 0; i <= input.length; i++) {
+    if (input[i] >= 1) {
+      a++;
+    } else if (input[i] <= -1) {
+      b += input[i];
+    }
+  }
 
-console.log(evenOrOdd(7));
+  return c.concat([a, b]);
+}
+console.log(countPositivesSumNegatives([]));
