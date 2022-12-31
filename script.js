3bff9c4646720e7aefb34bfc9bb9989cf40540fb
AShamshin <albert.shamshin@mail.ru>
2022-12-31 09:25:18 +0300

    makeUpperCase

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
-function countPositivesSumNegatives(input) {
-  let a = 0;
-  let b = 0;
-  let c = [];
-  if (input?.length === 0 || input === null) {
-    return [];
-  }
-  for (let i = 0; i <= input.length; i++) {
-    if (input[i] >= 1) {
-      a++;
-    } else if (input[i] <= -1) {
-      b += input[i];
-    }
-  }
-
-  return c.concat([a, b]);
+function makeUpperCase(str) {
+  return str.toUpperCase();
 }
-console.log(countPositivesSumNegatives([]));
+console.log(makeUpperCase('hello'));
