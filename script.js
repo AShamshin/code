9aaec081a7081abc1342cb2db28485c53ca6340d
AShamshin <albert.shamshin@mail.ru>
2023-01-01 11:14:04 +0300

    _if

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function whatday(num) {
-  if (num == 1) {
-    return 'Sunday';
-  } else if (num == 2) {
-    return 'Monday';
-  } else if (num == 3) {
-    return 'Tuesday';
-  } else if (num == 4) {
-    return 'Wednesday';
-  } else if (num == 5) {
-    return 'Thursday';
-  } else if (num == 6) {
-    return 'Friday';
-  } else if (num == 7) {
-    return 'Saturday';
-  } else {
-    return 'Wrong, please enter a number between 1 and 7';
-  }
+function _if(bool, func1, func2) {
+  return bool ? func1() : func2();
 }
-console.log(whatday(10));
