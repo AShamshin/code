8e448e4c0f9b2087dd3de768501a99670b919453
AShamshin <albert.shamshin@mail.ru>
2023-01-02 09:09:30 +0300

    basicOp

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function oddCount(n) {
-  return Math.floor(n / 2);
+function basicOp(operation, value1, value2) {
+  if (operation == '+') {
+    return value1 + value2;
+  } else if (operation == '-') {
+    return value1 - value2;
+  } else if (operation == '*') {
+    return value1 * value2;
+  } else if (operation == '/') {
+    return value1 / value2;
+  }
 }
+console.log(basicOp('+', 4, 7));
