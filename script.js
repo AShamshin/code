8ea998969742568f77ba5b0e341381002481311e
AShamshin <albert.shamshin@mail.ru>
2023-01-05 09:39:25 +0300

    nums

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
-// let firstNameId = 'first-name';
-// let lastNameId = 'last-name';
-// let addressId = 'address';
-// let citiesId = 'cities';
-// let hobbiesId = 'hobbies';
-// let avatarWrapperId = 'avatar-wrapper';
-// let avatarId = 'avatar';
-
-// let firstNameEl = document.getElementById(firstNameId);
-// let lastNameEl = document.getElementById(lastNameId);
-// let addressEl = document.getElementById(addressId);
-// let citiesEl = document.getElementById(citiesId);
-// let hobbiesEl = document.getElementById(hobbiesId);
-// let avatarWrapperEl = document.getElementById(avatarWrapperId);
-// let avatarEl = document.getElementById(avatarId);
-
-// alert(firstNameEl.className);
-// alert(lastNameEl.value);
-// alert(addressEl.value);
-// alert(citiesEl.innerHTML);
-// alert(hobbiesEl.innerHTML);
-// alert(avatarWrapperEl.innerHTML);
-// alert(avatarEl.alt);
-
-// firstNameEl.value = 'Sanek';
-// lastNameEl.setAttribute('value', 'Petrov');
-// lastNameEl.className = 'last-name default-input error-input';
-
-// function invert(array) {
-//   let arr = array.map((item) => (item ? item * -1 : -0));
-//   return arr;
-// }
-
-// console.log(invert([-1, -2, -3, -4, -5]));
-
-function invert(array) {
-  let arr = array.map((item) => (item ? item * -1 : -0));
-  return arr;
+function solution(nums) {
+  return nums !== null && nums.length > 1 ? nums.sort((a, b) => a - b) : [];
 }
-
-console.log(invert([-1, -2, -3, -4, -5]));
+console.log(solution(null));
