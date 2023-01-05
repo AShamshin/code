bfb2c19dcbce97bc9de518b5fdce2e83d7076bc9
AShamshin <albert.shamshin@mail.ru>
2023-01-05 10:17:46 +0300

    largest

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function solution(nums) {
-  return nums !== null && nums.length > 1 ? nums.sort((a, b) => a - b) : [];
+function largest(n, xs) {
+  if (n > 0) {
+    return xs.sort((a, b) => a - b), xs.slice(-n);
+  } else {
+    return [];
+  }
 }
-console.log(solution(null));
+console.log(largest(0, [9, 1, 50, 22, 3, 13, 2, 63, 5]));
