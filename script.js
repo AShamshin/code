76d7fac9214a383a5138df75178dd4064383842e
AShamshin <albert.shamshin@mail.ru>
2023-01-11 09:58:46 +0300

    calculator 2

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-function remainder(n, m) {
-  return Math.max(n, m) % Math.min(n, m);
+let buttonPlus = document.getElementById('buttonPlus');
+let buttonMinus = document.getElementById('buttonMinus');
+let buttonMultiply = document.getElementById('buttonMultiply');
+let buttonDivide = document.getElementById('buttonDivide');
+
+function onButtonPlusClick() {
+  let input1 = document.getElementById('number1');
+  let input2 = document.getElementById('number2');
+  let number1 = Number(input1.value);
+  let number2 = Number(input2.value);
+  let summa = number1 + number2;
+  alert(summa);
 }
-console.log(remainder(0, 1));
+function onButtonMinusClick() {
+  let input1 = document.getElementById('number1');
+  let input2 = document.getElementById('number2');
+  let number1 = Number(input1.value);
+  let number2 = Number(input2.value);
+  let summa = number1 - number2;
+  alert(summa);
+}
+function onButtonMultiplyClick() {
+  let input1 = document.getElementById('number1');
+  let input2 = document.getElementById('number2');
+  let number1 = Number(input1.value);
+  let number2 = Number(input2.value);
+  let summa = number1 * number2;
+  alert(summa);
+}
+function onButtonDivideClick() {
+  let input1 = document.getElementById('number1');
+  let input2 = document.getElementById('number2');
+  let number1 = Number(input1.value);
+  let number2 = Number(input2.value);
+  let summa = number1 / number2;
+  alert(summa);
+}
+buttonPlus.addEventListener('click', onButtonPlusClick);
+buttonMinus.addEventListener('click', onButtonMinusClick);
+buttonMultiply.addEventListener('click', onButtonMultiplyClick);
+buttonDivide.addEventListener('click', onButtonDivideClick);
