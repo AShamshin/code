88abe6f4d1e282bad5ee344a25059f02abcd4c6e
AShamshin <albert.shamshin@mail.ru>
2023-01-12 18:14:25 +0300

    updateLight

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-let operationButtons = document.getElementsByClassName('operation-button');
-
-let input1 = document.getElementById('number1');
-let input2 = document.getElementById('number2');
-
-function makeOperation(operationCode) {
-  let number1 = Number(input1.value);
-  let number2 = Number(input2.value);
-
-  let result;
-  if (operationCode === '+') {
-    result = number1 + number2;
-  } else if (operationCode === '-') {
-    result = number1 - number2;
-  } else if (operationCode === '*') {
-    result = number1 * number2;
-  } else if (operationCode === '/') {
-    result = number1 / number2;
-  } else {
-    alert('operation is unknown');
+function updateLight(current) {
+  if (current == 'green') {
+    return 'yellow';
+  } else if (current == 'yellow') {
+    return 'red';
+  } else if (current == 'red') {
+    return 'green';
   }
-  alert(result);
 }
-
-function onOperationButtonClick(eventObject) {
-  let clickedElement = eventObject.currentTarget;
-  let operation = clickedElement.innerHTML;
-  makeOperation(operation);
-}
-
-for (let i = 0; i < operationButtons.length; i++) {
-  let button = operationButtons[i];
-  button.addEventListener('click', onOperationButtonClick);
-}
+console.log(updateLight('green'));
