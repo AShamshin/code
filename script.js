d7288b20b4b48ca71e0fdae4c0a40bf32d25ed61
AShamshin <albert.shamshin@mail.ru>
2023-01-13 16:18:18 +0300

    myMap

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function mystery() {
-  var results = { sanity: 'Hello' };
-  return;
-}
+const bloggers = [
+  { name: 'Alex', age: 20 },
+  { name: 'Petter', age: 22 },
+  { name: 'Hanna', age: 25 },
+];
+
+const myBestMap = (arr, cllBack) => {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    const result = cllBack(element);
+    newArr.push(result);
+  }
+  return newArr;
+};
+const result = myBestMap(bloggers, (element) => {
+  return element.age * 2;
+});
+
+console.log(result);
