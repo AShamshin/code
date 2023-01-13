b8460264b05eb199815b6f0951c024fac0b0488d
AShamshin <albert.shamshin@mail.ru>
2023-01-13 19:03:55 +0300

    itk-slider

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-const bloggers = [
-  { name: 'Alex', age: 20 },
-  { name: 'Petter', age: 22 },
-  { name: 'Hanna', age: 25 },
-];
+let showPrevBtn = document.getElementById('show-prev-btn');
+let showNextBtn = document.getElementById('show-next-btn');
+let slideImage = document.getElementById('slide-amg');
 
-const myBestMap = (arr, cllBack) => {
-  const newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    const element = arr[i];
-    const result = cllBack(element);
-    newArr.push(result);
-  }
-  return newArr;
-};
-const result = myBestMap(bloggers, (element) => {
-  return element.age * 2;
-});
+showPrevBtn.addEventListener('click', onShowPrevBtnClick);
+showNextBtn.addEventListener('click', onShowNextBtnClick);
 
-console.log(result);
+let imagesUrls = [];
+imagesUrls.push('https://st1.zr.ru/_ah/img/t0lRQDylSmR94uiIbN9nZQ=s800');
+imagesUrls.push(
+  'https://static.sobaka.ru/images/image/01/51/31/09/_normal.jpg'
+);
+imagesUrls.push(
+  'https://chegevara29.ru/wp-content/uploads/2016/08/PAZ-320302.jpg'
+);
+imagesUrls.push(
+  'https://auto.vercity.ru/img/magazine/2019/06/19/1560966534.jpg'
+);
+
+let currentImageIndex = 0;
+
+slideImage.src = imagesUrls[currentImageIndex];
+
+function onShowPrevBtnClick() {
+  currentImageIndex--;
+  slideImage.src = imagesUrls[currentImageIndex];
+}
+function onShowNextBtnClick() {
+  currentImageIndex++;
+  slideImage.src = imagesUrls[currentImageIndex];
+}
