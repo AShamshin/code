c7670f2523b30ea2f5bb333afeb6bc863e45ee63
AShamshin <albert.shamshin@mail.ru>
2023-01-14 14:13:38 +0300

    sliderFactory

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-slider1.start();
+let slider1 = sliderFactory.getNewSlider();
+let slider2 = sliderFactory.getNewSlider();
+
+slider1.start('itk-slider1');
+slider2.start('itk-slider2');
--- slider.js
+++ slider.js
@@ -1,49 +1,125 @@
-let slider1 = {
-  imagesUrls: [],
-  currentImageIndex: 0,
-  showPrevBtn: document.getElementById('show-prev-btn'),
-  showNextBtn: document.getElementById('show-next-btn'),
-  slideImage: document.getElementById('slide-amg'),
+let sliderFactory = {
+  getNewSlider: function () {
+    let newSlider = {
+      imagesUrls: [],
+      currentImageIndex: 0,
 
-  start: function () {
-    let that = this;
-    this.showPrevBtn.addEventListener('click', function (e) {
-      that.onShowPrevBtnClick(e);
-    });
-    this.showNextBtn.addEventListener('click', function (e) {
-      that.onShowNextBtnClick(e);
-    });
+      showPrevBtn: null,
+      showNextBtn: null,
+      slideImage: null,
 
-    this.imagesUrls.push(
-      'https://st1.zr.ru/_ah/img/t0lRQDylSmR94uiIbN9nZQ=s800'
-    );
-    this.imagesUrls.push(
-      'https://static.sobaka.ru/images/image/01/51/31/09/_normal.jpg'
-    );
-    this.imagesUrls.push(
-      'https://chegevara29.ru/wp-content/uploads/2016/08/PAZ-320302.jpg'
-    );
-    this.imagesUrls.push(
-      'https://auto.vercity.ru/img/magazine/2019/06/19/1560966534.jpg'
-    );
-    this.slideImage.src = this.imagesUrls[this.currentImageIndex];
-    this.showPrevBtn.disabled = true;
-  },
+      start: function (elId) {
+        let that = this;
 
-  onShowPrevBtnClick: function (e) {
-    this.currentImageIndex--;
-    this.slideImage.src = this.imagesUrls[this.currentImageIndex];
-    this.showNextBtn.disabled = false;
-    if (this.currentImageIndex === 0) {
-      this.showPrevBtn.disabled = true;
-    }
-  },
-  onShowNextBtnClick: function (e) {
-    this.currentImageIndex++;
-    this.slideImage.src = this.imagesUrls[this.currentImageIndex];
-    this.showPrevBtn.disabled = false;
-    if (this.currentImageIndex === this.imagesUrls.length - 1) {
-      this.showNextBtn.disabled = true;
-    }
+        let elSelector = '#' + elId;
+        let el = document.querySelector(elSelector);
+
+        this.showPrevBtn = el.querySelector('.show-prev-btn');
+        this.showNextBtn = el.querySelector('.show-next-btn');
+        this.slideImage = el.querySelector('.slide-img');
+
+        this.showPrevBtn.addEventListener('click', function (e) {
+          that.onShowPrevBtnClick(e);
+        });
+        this.showNextBtn.addEventListener('click', function (e) {
+          that.onShowNextBtnClick(e);
+        });
+
+        this.imagesUrls.push(
+          'https://st1.zr.ru/_ah/img/t0lRQDylSmR94uiIbN9nZQ=s800'
+        );
+        this.imagesUrls.push(
+          'https://static.sobaka.ru/images/image/01/51/31/09/_normal.jpg'
+        );
+        this.imagesUrls.push(
+          'https://chegevara29.ru/wp-content/uploads/2016/08/PAZ-320302.jpg'
+        );
+        this.imagesUrls.push(
+          'https://auto.vercity.ru/img/magazine/2019/06/19/1560966534.jpg'
+        );
+        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+        this.showPrevBtn.disabled = true;
+      },
+
+      onShowPrevBtnClick: function (e) {
+        this.currentImageIndex--;
+        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+        this.showNextBtn.disabled = false;
+        if (this.currentImageIndex === 0) {
+          this.showPrevBtn.disabled = true;
+        }
+      },
+      onShowNextBtnClick: function (e) {
+        this.currentImageIndex++;
+        this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+        this.showPrevBtn.disabled = false;
+        if (this.currentImageIndex === this.imagesUrls.length - 1) {
+          this.showNextBtn.disabled = true;
+        }
+      },
+    };
+    return newSlider;
   },
 };
+
+function getNewSlider() {
+  let newSlider = {
+    imagesUrls: [],
+    currentImageIndex: 0,
+
+    showPrevBtn: null,
+    showNextBtn: null,
+    slideImage: null,
+
+    start: function (elId) {
+      let that = this;
+
+      let elSelector = '#' + elId;
+      let el = document.querySelector(elSelector);
+
+      this.showPrevBtn = el.querySelector('.show-prev-btn');
+      this.showNextBtn = el.querySelector('.show-next-btn');
+      this.slideImage = el.querySelector('.slide-img');
+
+      this.showPrevBtn.addEventListener('click', function (e) {
+        that.onShowPrevBtnClick(e);
+      });
+      this.showNextBtn.addEventListener('click', function (e) {
+        that.onShowNextBtnClick(e);
+      });
+
+      this.imagesUrls.push(
+        'https://st1.zr.ru/_ah/img/t0lRQDylSmR94uiIbN9nZQ=s800'
+      );
+      this.imagesUrls.push(
+        'https://static.sobaka.ru/images/image/01/51/31/09/_normal.jpg'
+      );
+      this.imagesUrls.push(
+        'https://chegevara29.ru/wp-content/uploads/2016/08/PAZ-320302.jpg'
+      );
+      this.imagesUrls.push(
+        'https://auto.vercity.ru/img/magazine/2019/06/19/1560966534.jpg'
+      );
+      this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+      this.showPrevBtn.disabled = true;
+    },
+
+    onShowPrevBtnClick: function (e) {
+      this.currentImageIndex--;
+      this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+      this.showNextBtn.disabled = false;
+      if (this.currentImageIndex === 0) {
+        this.showPrevBtn.disabled = true;
+      }
+    },
+    onShowNextBtnClick: function (e) {
+      this.currentImageIndex++;
+      this.slideImage.src = this.imagesUrls[this.currentImageIndex];
+      this.showPrevBtn.disabled = false;
+      if (this.currentImageIndex === this.imagesUrls.length - 1) {
+        this.showNextBtn.disabled = true;
+      }
+    },
+  };
+  return newSlider;
+}
