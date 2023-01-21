d6afddf684ed3a90326ef20a4f2efc541867cbda
AShamshin <albert.shamshin@mail.ru>
2023-01-21 16:07:39 +0300

    Todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <Todolist />
+      <Todolist />
+      <Todolist />
     </div>
   );
 }
 function Todolist() {
-  return <div>TODOLIST</div>;
+  return (
+    <div>
+      <h3>What to learn</h3>
+      <div>
+        <input type='text' />
+        <button>+</button>
+      </div>
+      <ul>
+        <li>
+          <input type='checkbox' checked={true} /> <span>CSS</span>
+        </li>
+        <li>
+          <input type='checkbox' checked={true} /> <span>JS</span>
+        </li>
+        <li>
+          <input type='checkbox' checked={false} /> <span>React</span>
+        </li>
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  );
 }
 export default App;
