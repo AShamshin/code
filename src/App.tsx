eface224e5d9f457dd39598b60dbbf6fbb5c3388
AShamshin <albert.shamshin@mail.ru>
2023-01-21 17:19:00 +0300

    Todolist.props

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,23 @@
 import './App.css';
+import { TaskType, Todolist } from './Todolist';
 
 function App() {
+  let tasks1: Array<TaskType> = [
+    { id: 1, title: 'CSS', isDone: true },
+    { id: 2, title: 'JS', isDone: true },
+    { id: 3, title: 'React', isDone: true },
+  ];
+  let tasks2: Array<TaskType> = [
+    { id: 1, title: 'Terminator', isDone: true },
+    { id: 2, title: 'XXX', isDone: false },
+    { id: 3, title: 'Jentlments of fortune', isDone: true },
+  ];
   return (
     <div className='App'>
-      <Todolist />
-      <Todolist />
-      <Todolist />
+      <Todolist title='What to learn' tasks={tasks1} />
+      <Todolist title='Movies' tasks={tasks2} />
     </div>
   );
 }
-function Todolist() {
-  return (
-    <div>
-      <h3>What to learn</h3>
-      <div>
-        <input type='text' />
-        <button>+</button>
-      </div>
-      <ul>
-        <li>
-          <input type='checkbox' checked={true} /> <span>CSS</span>
-        </li>
-        <li>
-          <input type='checkbox' checked={true} /> <span>JS</span>
-        </li>
-        <li>
-          <input type='checkbox' checked={false} /> <span>React</span>
-        </li>
-      </ul>
-      <div>
-        <button>All</button>
-        <button>Active</button>
-        <button>Completed</button>
-      </div>
-    </div>
-  );
-}
+
 export default App;
