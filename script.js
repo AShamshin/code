2dff851028a5a1ae69f4419e02db5ec21311645f
AShamshin <albert.shamshin@mail.ru>
2023-01-21 09:53:57 +0300

    TODOLIST

--- script.js
+++
@@ -1,8 +0,0 @@
-let count = 0;
-
-function cc(card) {
-  if (card <= 6) count++;
-  else if (card >= '10') count--;
-  return count + (count > 0 ? ' Bet' : ' Hold');
-}
-console.log(cc('10'));
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Todolist />
+    </div>
+  );
+}
+function Todolist() {
+  return <div>TODOLIST</div>;
+}
+export default App;
