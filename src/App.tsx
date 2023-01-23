f82b9509654b207fb28b9b7d753da69e62934e03
AShamshin <albert.shamshin@mail.ru>
2023-01-23 17:31:03 +0300

    Todolist useState 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,13 @@ import './App.css';
 import { TaskType, Todolist } from './Todolist';
 
 function App() {
-  let initTasks: Array<TaskType> = [
+  let [tasks, setTasks] = useState([
     { id: 1, title: 'CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
     { id: 3, title: 'React', isDone: true },
-  ];
+    { id: 3, title: 'Redux', isDone: true },
+  ]);
 
-  let arr = useState(initTasks);
-  let tasks = arr[0];
-  let setTasks = arr[1];
   function removeTask(id: number) {
     let filteredTasks = tasks.filter((t) => t.id !== id);
     setTasks(filteredTasks);
