0f5e0f264dae5c9e5bd0b74a21c11128f1ee0389
AShamshin <albert.shamshin@mail.ru>
2023-02-03 10:07:01 +0300

    booleanToString

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function grow(x) {
-  let b = 1;
-
-  for (let a = 0; a < x.length; a++) {
-    b *= x[a];
-  }
-  return b;
+function booleanToString(b) {
+  return String(b);
 }
-console.log(grow([4, 1, 1, 1, 5]));
+console.log(booleanToString(true));
