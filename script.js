a2d47ae02e8afc0b33212dc618e394d5b11f9fb9
AShamshin <albert.shamshin@mail.ru>
2023-02-05 12:26:44 +0300

    min max

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-function isPythagoreanTriple(integers) {
-  integers.sort((a, b) => a - b);
+var min = function (list) {
+  list.sort((a, b) => a - b);
 
-  return (
-    integers[0] * integers[0] + integers[1] * integers[1] ===
-    integers[2] * integers[2]
-  );
-}
-console.log(isPythagoreanTriple([3, 5, 9]));
+  return list[0];
+};
+
+var max = function (list) {
+  list.sort((a, b) => b - a);
+  return list;
+};
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110]));
+console.log(max([4, 6, 2, 1, 9, 63, -134, 566]));
