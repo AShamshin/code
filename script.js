b1a99a32a096088f107ee38d2d781b80df2f06d2
AShamshin <albert.shamshin@mail.ru>
2023-02-08 12:55:58 +0300

    numberToPower

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-var min = function (list) {
-  list.sort((a, b) => a - b);
-
-  return list[0];
-};
-
-var max = function (list) {
-  list.sort((a, b) => b - a);
-  return list;
-};
-
-console.log(min([-52, 56, 30, 29, -54, 0, -110]));
-console.log(max([4, 6, 2, 1, 9, 63, -134, 566]));
+function numberToPower(number, power) {
+  let a = 1;
+  for (let i = 0; i < power; i++) {
+    a *= number;
+  }
+  return a;
+}
+console.log(numberToPower(10, 4));
