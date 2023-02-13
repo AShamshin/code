4fd41b8676b911e64be0cc0cd969fdab1642c69e
AShamshin <albert.shamshin@mail.ru>
2023-02-13 09:01:59 +0300

    my map()

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
-function include(arr, item) {
-  return arr.includes(item);
+let students = [
+  {
+    name: 'Bob',
+    age: 22,
+    isMarried: true,
+    scores: 85,
+  },
+  {
+    name: 'Alex',
+    age: 21,
+    isMarried: true,
+    scores: 89,
+  },
+  {
+    name: 'Nick',
+    age: 20,
+    isMarried: false,
+    scores: 120,
+  },
+  {
+    name: 'John',
+    age: 19,
+    isMarried: false,
+    scores: 100,
+  },
+];
+
+function detNameS(array, f) {
+  let array2 = [];
+
+  for (let i = 0; i < array.length; i++) {
+    array2[i] = f(array[i]);
+  }
+  return array2;
 }
-console.log(include([1, 2, 3, 4], 7));
+
+abs = (obj) => {
+  let copy = { ...obj };
+  copy.scores = obj.scores + 1000;
+
+  return copy;
+};
+console.log(detNameS(students, abs));
