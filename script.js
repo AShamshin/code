2a076c8fb9f4ece56be6dc0801909fad7b37f7f0
AShamshin <albert.shamshin@mail.ru>
2023-02-19 18:26:51 +0300

    strCount

--- script.js
+++ script.js
@@ -1,43 +1,8 @@
-let students = [
-  {
-    name: 'Bob',
-    age: 22,
-    isMarried: true,
-    scores: 85,
-  },
-  {
-    name: 'Alex',
-    age: 21,
-    isMarried: true,
-    scores: 89,
-  },
-  {
-    name: 'Nick',
-    age: 20,
-    isMarried: false,
-    scores: 120,
-  },
-  {
-    name: 'John',
-    age: 19,
-    isMarried: false,
-    scores: 100,
-  },
-];
-
-function detNameS(array, f) {
-  let array2 = [];
-
-  for (let i = 0; i < array.length; i++) {
-    array2[i] = f(array[i]);
-  }
-  return array2;
+function strCount(str, letter) {
+  let result = str.split('').filter((s) => {
+    return s === letter;
+  });
+  return result.length;
 }
 
-abs = (obj) => {
-  let copy = { ...obj };
-  copy.scores = obj.scores + 1000;
-
-  return copy;
-};
-console.log(detNameS(students, abs));
+console.log(strCount('Hello', 'l'));
