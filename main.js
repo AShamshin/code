ffd7d72bc073f51f5672cd2fb907f6a2ec654731
AShamshin <albert.shamshin@mail.ru>
2023-02-27 12:13:35 +0300

    countSheeps

---
+++ main.js
@@ -0,0 +1,33 @@
+function countSheeps(arrayOfSheep) {
+  arrayOfSheep = arrayOfSheep.filter((f) => (f ? f : f));
+  return arrayOfSheep.length;
+}
+
+console.log(
+  countSheeps([
+    true,
+    true,
+    true,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    true,
+    true,
+  ])
+);
--- script.js
+++
@@ -1,4 +0,0 @@
-const areaOrPerimeter = function (l, w) {
-  return l === w ? l * w : 2 * (l + w);
-};
-console.log(areaOrPerimeter(6, 10));
