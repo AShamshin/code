6636a91a5deeed2754359d3c093a80c95b53f506
AShamshin <albert.shamshin@mail.ru>
2023-03-25 18:41:00 +0300

    solution

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-function divCon(x) {
-  let a = x.filter((el) => Number.isInteger(el));
-  let b = x.filter((el) => !Number.isInteger(el));
-  let c = 0;
-  let d = 0;
-  for (let i = 0; i < a.length; i++) {
-    c += a[i];
-  }
-
-  for (let i = 0; i < b.length; i++) {
-    d += +b[i];
-  }
-  return c - d;
+function solution(str, ending) {
+  return str.endsWith(ending);
 }
-
-console.log(divCon([9, 3, '7', '3']));
+console.log(solution('abcde', 'cde'));
