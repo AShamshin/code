cfd7aba0b1f5a252e9afa75624351f0dc49efe3f
AShamshin <albert.shamshin@mail.ru>
2023-03-25 19:22:37 +0300

    trim

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-var greet = function (name) {
-  let a = name.toLowerCase();
+function trim(str, size) {
+  let a = '';
 
-  let newStr = a[0].toUpperCase() + a.slice(1);
-
-  return 'Hello ' + newStr + '!';
-};
-console.log(greet('BILLY'));
+  if (str.length > size && size <= 3) {
+    a = str.substr(0, size) + '...';
+  } else if (str.length > size) {
+    a = str.substr(0, size - 3) + '...';
+  } else if (str.length <= size) {
+    a = str;
+  }
+  return a;
+}
+console.log(trim('Creating kata is fun', 14));
