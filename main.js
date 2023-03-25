e2f0d773940fbc22e459d5cccceac7fe93307c64
AShamshin <albert.shamshin@mail.ru>
2023-03-25 19:01:22 +0300

    greet

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-function solution(str, ending) {
-  return str.endsWith(ending);
-}
-console.log(solution('abcde', 'cde'));
+var greet = function (name) {
+  let a = name.toLowerCase();
+
+  let newStr = a[0].toUpperCase() + a.slice(1);
+
+  return 'Hello ' + newStr + '!';
+};
+console.log(greet('BILLY'));
